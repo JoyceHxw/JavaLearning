4aa3b1fa6a7abd5036ed6ae4e144fc5867664b0a
JoyceHxw <114238035+JoyceHxw@users.noreply.github.com>
2022-09-26 22:01:43 +0800

    Create helloworld.java

---
+++ day01/helloworld.java
@@ -0,0 +1,9 @@
+class HelloChina{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("姓名：");
+        System.out.println();
+        System.out.println("性别：男");
+        System.out.println("家庭住址：北京");
+    }
+}
